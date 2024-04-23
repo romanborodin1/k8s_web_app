789115eb51c79f147cea5a5fe0e3103acc910377
Roman Borodin <borodinrs1@gmail.com>
2024-04-24 01:01:33 +0300

    index.php - minor fix

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ $ip_server = $_SERVER['SERVER_ADDR'];
 $app_version = '1.0.0';
 
 echo "<h1>Welcome to the Simple Website based on PHP</h1>";
-echo "<h2>Created by <font color=yellow>Roman Borodin</font></h2>";
+echo "<h2>Created by <font color=orange>Roman Borodin</font></h2>";
 echo "<p>Your IP address is: $ip_server</p>";
 echo "<p>Application version: $app_version</p>";
 ?>
