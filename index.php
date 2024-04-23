790b26cde29e18f7fc872549e2e6c9dd9327902b
Roman Borodin <borodinrs1@gmail.com>
2024-04-24 01:05:08 +0300

    index.php - set background-color:yellow

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
 $ip_server = $_SERVER['SERVER_ADDR'];
-$app_version = '1.0.1';
+$app_version = '1.0.2';
 
+echo "<body style='background-color:yellow'>";
 echo "<h1>Welcome to the Simple Website based on PHP</h1>";
 echo "<h2>Created by <font color=orange>Roman Borodin</font></h2>";
 echo "<p>Your IP address is: $ip_server</p>";
