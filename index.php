43b22d1c3a0760bfcfbe13fd56e94f48e9f98495
Roman Borodin <borodinrs1@gmail.com>
2024-04-24 01:14:12 +0300

    index.php - play with colors

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
 $ip_server = $_SERVER['SERVER_ADDR'];
-$app_version = '1.0.2';
+$app_version = '1.0.3';
 
-echo "<body style='background-color:yellow'>";
+echo "<body style='background-color:#99FF11'>";
 echo "<h1>Welcome to the Simple Website based on PHP</h1>";
-echo "<h2>Created by <font color=orange>Roman Borodin</font></h2>";
+echo "<h2>Created by <font color=yellow>Roman Borodin</font></h2>";
 echo "<p>Your IP address is: $ip_server</p>";
 echo "<p>Application version: $app_version</p>";
 ?>
