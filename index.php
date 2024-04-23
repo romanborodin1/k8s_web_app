b2f7f30c7f635d12ac12bfb41c21f83b7bd20df2
Roman Borodin <borodinrs1@gmail.com>
2024-04-24 01:34:41 +0300

    index.php - simple HTML code

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php
+<!-- <?php
 $ip_server = $_SERVER['SERVER_ADDR'];
 $app_version = '1.0.3';
 
@@ -7,4 +7,20 @@ echo "<h1>Welcome to the Simple Website based on PHP</h1>";
 echo "<h2>Created by <font color=green>Roman Borodin</font></h2>";
 echo "<p>Your IP address is: $ip_server</p>";
 echo "<p>Application version: $app_version</p>";
-?>
+?> -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie-edge">
+    <title>TEST_PAGE</title>
+</head>
+<body>
+    <h1>Test page</h1>
+    <p>Test paragraph</p>
+    <p>Test paragraph2</p>
+</body>
+</html>
+
