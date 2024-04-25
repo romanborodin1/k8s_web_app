65e44927ac391229bee5474e1a1a5945ea77ee98
Roman Borodin <borodinrs1@gmail.com>
2024-04-26 00:13:42 +0300

    index.php - echo date()

--- index.php
+++ index.php
@@ -1,27 +1,17 @@
-<!-- <?php
-$ip_server = $_SERVER['SERVER_ADDR'];
-$app_version = '1.0.3';
-
-echo "<body style='background-color:#99FF11'>";
-echo "<h1>Welcome to the Simple Website based on PHP</h1>";
-echo "<h2>Created by <font color=green>Roman Borodin</font></h2>";
-echo "<p>Your IP address is: $ip_server</p>";
-echo "<p>Application version: $app_version</p>";
-?> -->
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie-edge">
-    <title>TEST_PAGE</title>
+    <title><?= "new page"; ?></title>
 </head>
 <body>
     <h1>Test page</h1>
     <p>Test paragraph</p>
     <?php
-        echo "Hello world from PHP!";
+        echo "<p>Текущая дата и время:</p>";
+        echo date(DATE_RSS);
     ?>
     <p>Test paragraph2</p>
 </body>
