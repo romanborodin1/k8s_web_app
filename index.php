201ff0f2b4ce9a60e78d8b37a32e04ef60abdf5d
Roman Borodin <borodinrs1@gmail.com>
2024-04-28 00:57:45 +0300

    index.php - lesson 5. Variables

--- index.php
+++ index.php
@@ -3,15 +3,23 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie-edge">
-    <title>Урок 4</title>
+    <title>Урок 5</title>
 </head>
 <body> 
     <?php
-        echo "Главный скрипт<br/>";
-        include 'second.php';       // В случае с include при отсутствии скрипта будет выведено предупреждение
-        // require 'second.php';    // В случае с require при отсутствии скрипта будет поднято исключение 
-        echo "Главный скрипт<br/>";
+        $abc_abc = 5;    // $ - обозначение переменной. = - присваивание значения
+        echo $abc_abc;    // Вывод на экран числа
+        echo $abc_abc - 3;
+        $abc = $defg = $hijk = 3;
+        echo $abc;
+        echo $defg;
+        echo $hijk;
+
+        $var1 = true;
+        $var2 = 10;
+        $var3 = 1.234;
+        $var4 = "string___112344";
+        $var5;    // NULL. Переменная не инициализирована
     ?>
 </body>
 </html>
