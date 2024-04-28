390f76f2e857446f3770c30e53e6d24baad9a4ed
Roman Borodin <borodinrs1@gmail.com>
2024-04-28 18:22:36 +0300

    index.php - lesson 6. Iintegers and floats

--- index.php
+++ index.php
@@ -3,23 +3,27 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Урок 5</title>
+    <title>Урок 6</title>
 </head>
 <body> 
-    <?php
-        $abc_abc = 5;    // $ - обозначение переменной. = - присваивание значения
-        echo $abc_abc;    // Вывод на экран числа
-        echo $abc_abc - 3;
-        $abc = $defg = $hijk = 3;
-        echo $abc;
-        echo $defg;
-        echo $hijk;
+<?php
+        // integers
+        $num = 1234;
+        $num = -12;
+        $num = 012;    // Число в восьмеричной системе счисления
+        $num = 0x12A;    // Число в шестнатиричной системе счисления
+        echo 07 + 03;    // 012, 10. Выведет в 10-тичной
+        echo "<br/>";
+	echo 0xA + 0xF;    // 25
+        echo "<br/>";
 
-        $var1 = true;
-        $var2 = 10;
-        $var3 = 1.234;
-        $var4 = "string___112344";
-        $var5;    // NULL. Переменная не инициализирована
+        // floats
+        $var = 1.23456;
+        $var = 1.23456e-3;    // Экспоненциальная запись. 1.23456 * 10^(-3)
+        echo $var;
+        echo "<br/>"; 
+        $var = 1.23456e+3;
+        echo $var;
     ?>
 </body>
 </html>
