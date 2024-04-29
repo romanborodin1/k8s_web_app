218b47ff346e4726aef29c42823a69d3e7a07c12
Roman Borodin <borodinrs1@gmail.com>
2024-04-29 19:20:37 +0300

    index.php - lesson 11. OOP, classes, objects. Add point.php

--- index.php
+++ index.php
@@ -3,44 +3,25 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Урок 10</title>
+    <title>Урок 11</title>
 </head>
 <body> 
     <?php
-    $tests = array(
-        "42",
-	1337,
-	0x539,
-        02471,
-        0b10100111001,
-        1337e0,
-        "0x539",
-        "0b10100111001",
-        "1337e0",
-        "not numeric",
-        array(),
-        9.1,
-        null
-    );
+        require 'point.php';
+        $point1 = new Point;
+        $point1->x = 13;
+        $point1->y = 2;
+        echo $point1->x;
+        echo "<br/>";
 
-    foreach ($tests as $element) {
-        if (is_numeric($element)) {    // Проверяем, является ли значение переменной числом
-            echo var_export($element, true) . " - число<br/>", PHP_EOL;
-	} else {
-            echo var_export($element, true) . " - НЕ число<br/>", PHP_EOL;
-	}         
-    }
+        $point2 = new Point();    // Можно создавать объект класса с круглыми скобками и без них
+        $point2->x = 11;
+        $point2->y = 47;
+	echo $point2->x;
+        echo "<br/>";
 
-    echo "<br/>";
-    $var = 21.2784562923479;
-    echo round($var, 2);    // Округляем
-    echo "<br/>";
-    echo ceil($var);    // Округляем вверх
-    echo "<br/>";
-    echo floor($var);    // Округляем вниз
-
-    echo "<br/>";
-    echo decbin(7);    // Привести из десятичной системы счисления в двоичную. Возвращает string
+	unset($point2);    // Удаляем объект класса Point, на который указывает переменная point2
+        echo $point2->x;
     ?>
 </body>
 </html>
