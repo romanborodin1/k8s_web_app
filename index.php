e5b3bbec94c814389931ea7a8d08bdc4f294ce3e
Roman Borodin <borodinrs1@gmail.com>
2024-04-29 23:07:40 +0300

    index.php - lesson 13. Distance between two points

--- index.php
+++ index.php
@@ -3,35 +3,39 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Урок 11</title>
+    <title>Урок 13</title>
 </head>
 <body> 
     <?php
         require 'point.php';
-	echo Point::$num;    // Обращение к статической переменной
-        echo "<br/>";
-
-        $first = $second = 2;
-        $first = 3;
-	echo $first . '<br/>' . $second;
-	echo "<br/>";
 
         $first_point = new Point;
         $first_point->x = 3;
 	$first_point->y = 3;
 
-	$second_point = $first_point;
+	$second_point = clone $first_point;    // Клонировать объект, а не сделать ссылку на него
         $second_point->x = 5;
         $second_point->y = 5;
 
-        echo "x: {$first_point->x}, y: {$first_point->y}<br/>";
+	echo "x: {$first_point->x}, y: {$first_point->y}<br/>";
+        echo "<br/>";
 	echo "x: {$second_point->x}, y: {$second_point->y}<br/>";
 
-        $num1 = 5;
-        $num2 = &$num1;    // Указываем ссылку на объект, который хранится в переменной $num1
-        $num2 = 333;
-	echo "num1: {$num1}<br/>";    // 333
-        
+        echo pow(2, 8);     // 2^8
+        echo "<br/>";
+        echo sqrt(16);     // Корень квадратный из 16
+	echo "<br/>";
+
+        $p1 = new Point;
+        $p1->x = 10;
+        $p1->y = 34;
+
+        $p2 = new Point;
+        $p2->x = 3;
+	$p2->y = 10;
+
+	$distance = sqrt((pow(($p2->x - $p1->x), 2) + pow(($p2->y - $p1->y), 2)));
+        echo $distance;
     ?>
 </body>
 </html>
