d647bffb2200b85dd17b064264f185d1da52b04e
Roman Borodin <borodinrs1@gmail.com>
2024-04-29 18:19:55 +0300

    index.php - lesson 10. Rounding numbers

--- index.php
+++ index.php
@@ -3,43 +3,44 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Урок 9</title>
+    <title>Урок 10</title>
 </head>
 <body> 
     <?php
-        $str = '45.2';
-        $number = $str - 12;    // Пример неявного приведения типов
-        echo $number;    // 33.2
-        echo '<br/>';
+    $tests = array(
+        "42",
+	1337,
+	0x539,
+        02471,
+        0b10100111001,
+        1337e0,
+        "0x539",
+        "0b10100111001",
+        "1337e0",
+        "not numeric",
+        array(),
+        9.1,
+        null
+    );
 
-        $str = '45.2_some_text_here_123';
-        $number = $str - 12;    // Пример неявного приведения типов
-        echo $number;    // 33.2. Всё текстового он отбросил
-        echo '<br/>';
+    foreach ($tests as $element) {
+        if (is_numeric($element)) {    // Проверяем, является ли значение переменной числом
+            echo var_export($element, true) . " - число<br/>", PHP_EOL;
+	} else {
+            echo var_export($element, true) . " - НЕ число<br/>", PHP_EOL;
+	}         
+    }
 
-        // $var = 0.0;    // -> false
-        // $var = 0;    // -> false
-        // $var = "0";    // -> false
-        if ($var) {
-            echo "Переменная $var рассматривается как true";    // Приведение к строковмоу типу
-        } else {
-            echo "Переменная $var рассматривается как false";
-        }
-        echo '<br/>';
+    echo "<br/>";
+    $var = 21.2784562923479;
+    echo round($var, 2);    // Округляем
+    echo "<br/>";
+    echo ceil($var);    // Округляем вверх
+    echo "<br/>";
+    echo floor($var);    // Округляем вниз
 
-        echo true;    // -> 1
-        echo '<br/>';
-        echo false;    // -> ""
-        echo '<br/>';
-
-        // Явное приведение типов:
-        $float = 4.3;
-        $number = (int)$float;    // Интерпретатор отбрасывает дробную часть и переводит вещесвтенное число в целое
-        echo $number;
-        echo '<br/>';
-        $num = 21;
-	$f = (float)($num/2) - (int)($num/2);    // Если будет 0, то число будет чётное
-        echo $f;
+    echo "<br/>";
+    echo decbin(7);    // Привести из десятичной системы счисления в двоичную. Возвращает string
     ?>
 </body>
 </html>
