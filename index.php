74241a9e0c2e94e3d562fa20e641d831ef11540d
Roman Borodin <borodinrs1@gmail.com>
2024-05-03 14:37:55 +0300

    index.php - lesson 22. Ternary operator

--- index.php
+++ index.php
@@ -3,25 +3,33 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Урок 21</title>
+    <title>Урок 22</title>
 </head>
 <body> 
     <?php
-        $num = 9;
-        if ($num > 0 && $num < 8) {
-            echo "0<{$num}<8<br/>";
+        $x = -450;
+        $x = $x < 0 ? -$x : $x;
+        echo $x . '<br>';
+
+        // Оператор ?? - проинициализировать переменную ТОЛЬКО в том случае, если она не была ранее проинициализирована
+        $y = '100';    // null - переменная НЕинициализирована
+        $y = $y ?? 1;
+        echo "{$y}<br>";
+
+        // Оператор goto - переход на метку в коде. 
+        // НЕ РЕКОМЕНДУЕТСЯ ЕГО ИСПОЛЬЗОВАТЬ!!! 
+        // Лучше использовать if-else и циклы
+        $n = 0;
+        echo "[1] n = {$n}<br>";
+        finish:    // Метка
+        echo "[_] n = {$n}<br>";
+
+        $n++;
+        if ($n > 6) {
+            goto end;
         }
-        if ($num > 0 || $num < 8) {
-            echo "Условие отработало<br/>";
-        }
-        if (!($num < 0)) {
-            echo "Условие отработало<br/>";
-        }
-        // $f = true;
-        $f = $num > 0;
-        if ($f) {
-            echo "Условие отработало<br/>";
-        }
+        goto finish;    // Переход на метку
+        end:
     ?>
 </body>
 </html>
