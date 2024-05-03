ee16ff6b995a285e3805a0151820d7db9224645b
Roman Borodin <borodinrs1@gmail.com>
2024-05-03 15:57:15 +0300

    index.php - lesson 23. Operator switch

--- index.php
+++ index.php
@@ -3,33 +3,59 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Урок 22</title>
+    <title>Урок 23</title>
 </head>
 <body> 
     <?php
-        $x = -450;
-        $x = $x < 0 ? -$x : $x;
-        echo $x . '<br>';
-
-        // Оператор ?? - проинициализировать переменную ТОЛЬКО в том случае, если она не была ранее проинициализирована
-        $y = '100';    // null - переменная НЕинициализирована
-        $y = $y ?? 1;
-        echo "{$y}<br>";
-
-        // Оператор goto - переход на метку в коде. 
-        // НЕ РЕКОМЕНДУЕТСЯ ЕГО ИСПОЛЬЗОВАТЬ!!! 
-        // Лучше использовать if-else и циклы
-        $n = 0;
-        echo "[1] n = {$n}<br>";
-        finish:    // Метка
-        echo "[_] n = {$n}<br>";
-
-        $n++;
-        if ($n > 6) {
-            goto end;
+        $char = "c++";
+        switch($char) {
+            case 'php':    // После ':' - блок кода для этого случая
+                ?>
+                <h1>Язык PHP</h1>
+                <?php
+                break;    // Чтобы указать, что наш случай завершился
+            case 'c#':
+                ?>
+                <h1>Язык C#</h1>
+                <?php
+                break;
+            case 'js':
+                ?>
+                <h1>Язык JavaScript</h1>
+                <?php
+                break;
+            default:
+                ?>
+                <h1>Неизвестный язык</h1>
+                <?php
+                break;
         }
-        goto finish;    // Переход на метку
-        end:
+
+        $num = 120;
+        if ($num > 0 && $num <= 10) {
+            echo "0 < $num <= 10 <br>";
+        } elseif ($num > 10 && $num <= 100) {
+            echo "10 < $num <= 100 <br>";
+        } elseif ($num > 100 && $num <= 1000) {
+            echo "100 < $num <= 1000 <br>";
+        } else {
+            echo "$num < 0 || $num > 1000 <br>";
+        }
+
+        switch (true) {    // Будем проверять на первую истину
+            case ($num > 0 && $num <= 10):
+                echo "0 < $num <= 10 <br>";
+                break;
+            case ($num > 10 && $num <= 100):
+                echo "10 < $num <= 100 <br>";
+                break;
+            case ($num > 100 && $num <= 1000):
+                echo "100 < $num <= 1000 <br>";
+                break;
+            default:
+                echo "$num < 0 || $num > 1000 <br>";
+                break;
+        }
     ?>
 </body>
 </html>
